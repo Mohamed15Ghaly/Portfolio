ec46a61d4b37f5c071ab5f7bae8dedd211ea2180
Mohamed15Ghaly <mohamedghaly07@gmail.com>
2025-07-15 20:04:30 +0300

    Update Flutter Web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1729171600"
+    serviceWorkerVersion: "2979754850"
   }
 });
